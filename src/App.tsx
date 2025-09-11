098b247ba0b95637bcd4aa36d0ee0943f015a40e
Andrea715457 <andrea715457@gmail.com>
2025-09-10 23:26:33 -0500

    React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import { Pokemon, Pokemons, Items } from './pages';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Pokemon</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/pokemons/:name" element={<Pokemon/>}/>
+          <Route path="/pokemons" element={<Pokemons/>}/>
+          <Route path="/items" element={<Items/>}/>
+          <Route path="/" element={<Pokemons/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Pokemons.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Pokemons = () => {
+    const { name } = useParams();
+    return <div>{name}</div>
+};
+
+export default Pokemons
